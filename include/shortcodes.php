52a2e7b949c42ae967269c55afa7dafb0b5d5a47
FarhadHP <mobcomtr@gmail.com>
2017-11-08 15:18:17 +0330

    update

--- include/shortcodes.php
+++ include/shortcodes.php
@@ -4,19 +4,19 @@ add_shortcode('swa_alexa_country', 'swa_alexa_country');
 add_shortcode('swa_alexa_country_name', 'swa_alexa_country_name');
 add_shortcode('swa_alexa_country_global', 'swa_alexa_country_global');
 
-// get_site_url()
+
 function swa_alexa_country(){
-	$source = simplexml_load_file('http://data.alexa.com/data?cli=10&url=farhadhp.ir');
+	$source = simplexml_load_file('http://data.alexa.com/data?cli=10&url='.get_site_url());
     return $source->SD->COUNTRY['RANK'];
 }
 
 function swa_alexa_country_name(){
-	$source = simplexml_load_file('http://data.alexa.com/data?cli=10&url=farhadhp.ir');
+	$source = simplexml_load_file('http://data.alexa.com/data?cli=10&url='.get_site_url());
     return $source->SD->COUNTRY['NAME'];
 }
 
 function swa_alexa_country_global(){
-	$source = simplexml_load_file('http://data.alexa.com/data?cli=10&url=farhadhp.ir');
+	$source = simplexml_load_file('http://data.alexa.com/data?cli=10&url='.get_site_url());
     return $source->SD->POPULARITY['TEXT'];
 }
 
